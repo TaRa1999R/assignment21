de73add8026fdb7ef2311636ab6885a9b3b13fe1
TaRa1999R <rezaietara78@gmail.com>
2023-10-13 01:51:36 +0330

    assignment21

--- store/store.py
+++ store/store.py
@@ -8,11 +8,20 @@ def load_database () :
     cur = con.cursor ()
 
 def show_menue () :
-    print ("\n 1- Show Store's products")
+    print ("\n 1- Show store's products")
     print (" 2- Add new product ")
     print (" 3- Edit a products information ")
     print (" 4- Delete a product ")
     print (" 5- Exit \n")
+
+def add () :
+    ...
+
+def edit () :
+    ...
+
+def delete () :
+    ...
 
 print ("\n Hello! Welcome to my store 😊")
 print (" Loading ... ⏳")
@@ -21,19 +30,19 @@ print (" Data loaded ⌛")
 
 while True :
     show_menue ()
-    choice = input (" Enter the number what you want to do from the menue : ")
-    if choice == "1" or choice == "Show Store's products" :
+    choice = input (" Enter what you want to do from the menue : ")
+    if choice == "1" or choice == "Show store's products" :
         for data in cur.execute ("SELECT * FROM products ") :
             print ( data )
     
     elif choice == "2" or choice == "Add new product" :
-        ...
+        add ()
 
     elif choice == "3" or choice == "Edit a products information" :
-        ...
+        edit ()
 
     elif choice == "4" or choice == "Delete a product" :
-        ...
+        delete ()
     
     elif choice == "5" or choice == "Exit" :
         exit (0)
